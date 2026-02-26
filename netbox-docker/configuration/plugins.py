db27416aec1f8aa1ea4b08293f8d4b002c6e59ed
André Lemos <andretlemos@gmail.com>
2026-02-26 18:49:20 +0000

    Fix it deploying netbox with plugin netbox-bgp

--- netbox-docker/configuration/plugins.py
+++ netbox-docker/configuration/plugins.py
@@ -1,13 +1,1 @@
-# Add your plugins and plugin settings here.
-# Of course uncomment this file out.
-
-# To learn how to build images with your required plugins
-# See https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
-
-PLUGINS = ["netbox_bgp"]
-
-PLUGINS_CONFIG = {
-  "netbox_bgp": {
-    "top_level_menu": True
-  }
-}
+PLUGINS = ['netbox_bgp']
